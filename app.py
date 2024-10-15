368b6ab3d8de5ebbcaed2548c8d8827156ee5d1c
paul <pyupanquiv@hotmail.com>
2024-10-14 19:38:34 -0500

    Webhook

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
@@ -19,13 +19,6 @@ class Log(db.Model):
 #Crear la tabla si no existe
 with app.app_context():
     db.create_all()
-
-    prueba1=Log(texto='Mensaje de Prueba 1')
-    prueba2=Log(texto='Mensaje de Prueba 2')
-
-    db.session.add(prueba1)
-    db.session.add(prueba2)
-    db.session.commit()
 
 #Funcion para ordenar los registros por fecha y hora
 def  ordenar_por_fecha_y_hora(registros):
@@ -48,6 +41,33 @@ def index():
         nuevo_registro = Log(texto=texto)
         db.session.add(nuevo_registro)
         db.session.commit()
+
+#TOKEN DE VERIFICACION PARA LA CONFIGURACION 
+TOKEN_ANDERCODE = 'ANDERCODE'
+
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if request.method =='GET':
+        challenge= verificar_token(request)
+        return challenge
+    elif request.method== 'POST':
+        response = recibir_mensajes(request)
+        return response
+
+def verificar_token(req):
+    token=req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if challenge and token == TOKEN_ANDERCODE:
+        return challenge
+    else:
+        return jsonify({'error':'Token Invalido'}),401
+
+def recibir_mensajes(req):
+    req =request.get_json()
+    agregar_mensajes_log(req)
+    
+    return jsonify({'message':'EVENT_RECEIVED'})
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
